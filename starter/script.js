d0849eeaf98831ae8122a2c3865b4a7ea71ab5ab
attobra <nora.attobra@gmail.com>
2021-03-04 17:00:35 -0500

    second commit

--- starter/script.js
+++ starter/script.js
@@ -4,8 +4,13 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `You are ${age} born in ${birthYear} `;
+    const output = `${firstName}, You are ${age} born in ${birthYear} `;
     console.log(output);
+
+    if (birthYear >= 1991 && birthYear <= 1996) {
+      const str = `Oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+    }
   }
 
   printAge();
